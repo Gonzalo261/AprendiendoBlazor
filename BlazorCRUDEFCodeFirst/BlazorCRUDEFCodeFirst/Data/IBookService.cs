ee7b5fbfd081d7d123d741331790fef97fff099c
Gonzalo Ruiz <111589@tecnicatura.frc.utn.edu.ar>
2021-12-10 02:52:58 -0300

    PrimeraCarga

---
+++ BlazorCRUDEFCodeFirst/BlazorCRUDEFCodeFirst/Data/IBookService.cs
@@ -0,0 +1,18 @@
+﻿using BlazorCRUDEFCodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDEFCodeFirst.Data
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<Book>> GetAllBook();
+        Task<Book> GetBookDetails(int id);
+        Task<bool> InsertBook(Book book);
+        Task<bool> UpdateBook(Book book);
+        Task<bool> DeleteBook(int id);
+        Task<bool> SaveBook(Book book);
+    }
+}
---
+++ BlazorCRUDEFCodeFirst/BlazorCRUDEFCodeFirst/Models/MyBooksContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDEFCodeFirst.Models
+{
+    public class MyBooksContext :  DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-CQUI9AH\\SQLEXPRESS;Initial Catalog=BlazorCRUDEFCodeFirst;Persist Security Info=True;User ID=sa;Password=sa");
+        }
+    }
+}
